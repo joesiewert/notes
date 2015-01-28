c8d10d1950cac03bd59908d595f37e5df55de215
joesiewert <joe.siewert@gmail.com>
2015-01-28 16:24:52 -0700

    Can now create notes

---
+++ app/controllers/notes.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  actions: {
+    newNote: function() {
+      var body = this.get('noteCopy');
+
+      var note = this.store.createRecord('note', {body: body});
+      this.set('noteCopy', '');
+      note.save();
+    }
+  }
+});
